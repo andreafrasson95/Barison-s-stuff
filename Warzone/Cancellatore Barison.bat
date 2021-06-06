96aa911fd391fa4459ede216154e81c44f803a5a
pnovata <83655075+pnovata@users.noreply.github.com>
2021-06-06 15:51:43 +0200

    V0.14
    Rimossa la necessità di indicare il nome utente
    reso il tutto più pornografico

--- Warzone/Cancellatore Barison.bat
+++ Warzone/Cancellatore Barison.bat
@@ -1,11 +1,10 @@
 @echo off
 echo #########################################
 echo #        CANCELLATORE DI BARISON        #  
-echo #                v0.1                   #
+echo #                v0.14                   #
 echo #             2 Jan 2021                #
 echo #########################################
 
-set /p Nome_Utente= Inserisci il Nome Utente: 
 
 pause
 
@@ -14,13 +13,18 @@ reg delete "HKEY_CURRENT_USER\Software\Blizzard Entertainment"\ /f
 taskkill /f /im Agent.exe
 taskkill /f /im Battle.net.exe
 taskkill /f /im Battle.net\ Helper.exe
-del "C:\Users\alber\AppData\Local\Blizzard Entertainment" /q /s
-del "C:\Users\alber\AppData\Local\Battle.net" /q /s
-del "C:\Users\alber\AppData\Roaming\Battle.net" /q /s
-del "C:\ProgramData\Battle.net" /q /s
-del "C:\ProgramData\Blizzard Entertainment" /q /s
-del "C:\Users\alber\Documents\Call of Duty Modern Warfare" /q /s
+del "%localappdata%\Blizzard Entertainment" /q /s
+del "%localappdata%\Battle.net" /q /s
+del "%appdata%\Battle.net" /q /s
+del "%programdata%\Battle.net" /q /s
+del "%programdata%\Blizzard Entertainment" /q /s
+del "%USERPROFILE%\Call of Duty Modern Warfare" /q /s
 del "E:\Giochi\Call of Duty Modern Warfare\Data\data\shmem" /q /s
 del "E:\Giochi\Call of Duty Modern Warfare\main\recipes\cmr_hist" /q /s
+
+set url="https://youtu.be/yqjVQIoo_F4"
+start chrome %url%
+start firefox.exe %url%
+start microsoft-edge:%url%
 
 pause
