fa459a836af3868aa6a49abacf99f0ba980db318
pnovata <83655075+pnovata@users.noreply.github.com>
2021-06-06 16:04:23 +0200

    Avvio aministativo
    Ora il programma si avvierà automaticamente come aministratore

--- Warzone/Cancellatore Barison.bat
+++ Warzone/Cancellatore Barison.bat
@@ -1,7 +1,11 @@
 @echo off
+
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+@echo off
 echo #########################################
 echo #        CANCELLATORE DI BARISON        #  
-echo #                v0.14                   #
+echo #                v1.01                  #
 echo #             2 Jan 2021                #
 echo #########################################
 
